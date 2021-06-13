8aaea18e4e097af0e18f515a230c03e5337ef0f2
nikhil076 <nikhil.arvind7@gmail.com>
2021-06-13 21:01:34 +0530

    Refracted the file in UC5

--- employeeWage.sh
+++ employeeWage.sh
@@ -10,6 +10,7 @@ fi
 PerHourWage=20
 FullDayHour=8
 PartTimeHour=8
+WorkingPerMonth=20
 
 echo "Enter 1 to check for full time employee wages"
 echo "Enter 2 to check for part time employee wages"
@@ -17,11 +18,16 @@ read n
 case $n in
 
 	1)	EmployeeWage=$(($PerHourWage*$FullDayHour))
-		echo "Full time employee wage is $EmployeeWage" ;;
+		echo "Full time employee per day wage is $EmployeeWage"
+		FullTimeWagesPerMonth=$(($WorkingPerMonth*$EmployeeWage)) 
+		echo "Full time employee per month wage is $FullTimeWagesPerMonth" ;;
 
 
 	2)	PartTimeEmployeeWage=$(($PerHourWage*$PartTimeHour))
-		echo "part time employee wage is $PartTimeEmployeeWage" ;;
+		echo "part time employee per day wage is $PartTimeEmployeeWage"
+		PartTimeWagesPerMonth=$(($WorkingPerMonth*$PartTimeEmployeeWage))
+		echo  "Part time employee wage per month is $PartTimeWagesPerMonth" ;;
 
 	*)	echo "Enter correct number" ;;
 esac
+
